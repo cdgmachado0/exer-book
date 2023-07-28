7999bc3acb31f8e60aa4777747695d63b03edb4a
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-07-28 17:27:59 +0200

    Done with median

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,20 @@
+
+
+
 fn main() {
-    let mut list = vec![3, 5, 7, 9, 10, 14, 32];
+    let mut list = vec![3, 5, 7, 9, 10, 14, 32, 99];
     get_median(&mut list);
-    get_median2(&mut list);
 
 }
 
 
-fn get_median(list: &mut Vec<i32>) {
+fn get_median(list: &mut Vec<i32>) { 
     list.sort();
 
-    let mut median = list.len() / 2;
-    median -= 1;
-    let median = list[median];
+    let i = list.len() / 2;
+    let median = list[i];
 
     println!("The median value is: {}", median);
+    println!("vec: {:?}", list);
 }
-
-lazy_static! {
-    static ref LENGTH: i32 = 
-}
-
-fn get_median2(list: &mut Vec<i32>) {
-    let mut sum = 0;
-    struct Number {
-        num: i32,
-        remainder: i32,
-    }
-
-    let numbers: [Number; list.len()];
-
-    for num in list.iter() {
-        sum += num;
-    }
-
-    let half = sum / 2;
-
-    for num in list.iter() {
-        let remainder = half % num;
-
-        // let x = Number {
-        //     num: *num,
-        //     remainder: half % num,
-        // };
-    }
-}
-
-
-
-
 
