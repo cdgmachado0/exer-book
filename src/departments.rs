77bc9f0a3b0141ff365e5bcc1f1cb1c95f0333e3
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-07-30 02:05:01 +0200

    Happy path on addition done

--- src/departments.rs
+++ src/departments.rs
@@ -10,45 +10,58 @@ pub fn add_department() {
 
     let mut user_input = String::new();
 
-    if let Err(_) = io::stdin().read_line(&mut user_input) {
-        println!("error happened");
-        return;
-    };
+    loop {
+        println!("Please insert an action");
 
-    user_input = user_input.to_lowercase();
-
-    if !user_input.starts_with("add")  {
-        if !user_input.starts_with("remove") {
-            println!("Not a valid action. Valid is either ADD or REMOVE");
+        if let Err(_) = io::stdin().read_line(&mut user_input) {
+            println!("error happened");
             return;
+        };
+
+        user_input = user_input.to_lowercase();
+
+        if user_input == "list" {
+            show_employes(&company_record);
+            continue;
         }
-    } 
 
-    let dep_index = user_input.rfind(' ').unwrap();
+        if !user_input.starts_with("add")  {
+            if !user_input.starts_with("remove") {
+                println!("Not a valid action. Valid is either ADD or REMOVE");
+                return;
+            }
+        } 
 
-    for (i, c) in user_input.chars().enumerate() {
-        if i == 3 && c == ' ' {
-            let no_first_space = &user_input[4..];
-            let second_space_i = no_first_space.find(' ').unwrap();
-            let name = &no_first_space[..second_space_i];
+        
 
-            for employee in employees {
-                if name == employee {
-                    let no_second_space = &user_input[second_space_i ..];
-                    for dep in departments {
-                        if dep == no_second_space {
-                            edit_employee(name, dep, &mut company_record, true);
+        for (i, c) in user_input.chars().enumerate() {
+            if i == 3 && c == ' ' {
+                let no_first_space = &user_input[4..];
+                let second_space_i = no_first_space.find(' ').unwrap();
+                let name = &no_first_space[..second_space_i];
+
+                for employee in employees { 
+                    if name == employee {
+                        // let no_second_space = &no_first_space[second_space_i ..];
+                        let possible_dep = user_input.rfind(' ').unwrap();
+                        let possible_dep = user_input[possible_dep + 1..].trim();
+
+
+                        for dep in departments {
+                            if dep == possible_dep {
+                                edit_employee(name, dep, &mut company_record, true);
+                            }
                         }
                     }
-                }
-            };
+                };
+
+            }
+
+            if i == 6 && c == ' ' {
+
+            }
 
         }
-
-        if i == 6 && c == ' ' {
-
-        }
-
     }
 }
 
@@ -63,10 +76,15 @@ fn edit_employee(
         company_record
             .entry(name.to_string())
             .or_insert(department.to_string());
+        println!("Success! {name} added to {department}");
     } else {
         //remove
     }
+}
 
-
+fn show_employes(company_record: &HashMap<String, String>) {
+    for (employee, department) in company_record {
+        println!("{} - {}", employee, department);
+    }
 }
 
